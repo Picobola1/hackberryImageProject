a86cf57b00865d674ab4732163bfeff6e34f03b6
Ellie Bonilla <bonilla.ellie@gmail.com>
2025-09-25 21:54:29 -0700

    hehe i have canny working

--- Testing.py
+++ Testing.py
@@ -2,8 +2,16 @@ import cv2 as cv
 import numpy as np
 
 img = cv.imread('tiger.jpg')
-
+## show the image with edits
 cv.imshow('Tiger', img)
 
+## making it a grey scale
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+## show the image with edits
+cv.imshow('Gray', gray)
+
+canny = cv.Canny(img, 125 , 175)
+##         name of it     what ur chaning
+cv.imshow('Canny Image', canny)
 
 cv.waitKey(0)
