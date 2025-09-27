972932e7ee8fe1b54acfb1e92f669e043b4cce37
Ellie Bonilla <bonilla.ellie@gmail.com>
2025-09-26 18:21:29 -0700

    blur and less contours hehe now i am starting face recognition

--- Testing.py
+++ Testing.py
@@ -10,8 +10,20 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 ## show the image with edits
 cv.imshow('Gray', gray)
 
-canny = cv.Canny(img, 125 , 175)
+blur = cv.GaussianBlur(gray, (5,5), cv.BORDER_DEFAULT)
+cv.imshow('Blur', blur)
+
+
+canny = cv.Canny(blur, 125 , 175)
 ##         name of it     what ur chaning
 cv.imshow('Canny Image', canny)
 
+contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+print(f'{len(contours)} contour(s) found!')
+
+
+
+
 cv.waitKey(0)
+cv.destroyAllWindows()
+
