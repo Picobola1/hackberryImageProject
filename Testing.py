5eb913688058beef20bd95e15ac19eeaee9b140e
Ellie Bonilla <bonilla.ellie@gmail.com>
2025-09-26 18:29:35 -0700

    SomeTestImagesAndHaarFace

--- Testing.py
+++ Testing.py
@@ -1,25 +1,10 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('tiger.jpg')
+img = cv.imread('1.jpg')
 ## show the image with edits
-cv.imshow('Tiger', img)
+cv.imshow('Me', img)
 
-## making it a grey scale
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-## show the image with edits
-cv.imshow('Gray', gray)
-
-blur = cv.GaussianBlur(gray, (5,5), cv.BORDER_DEFAULT)
-cv.imshow('Blur', blur)
-
-
-canny = cv.Canny(blur, 125 , 175)
-##         name of it     what ur chaning
-cv.imshow('Canny Image', canny)
-
-contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-print(f'{len(contours)} contour(s) found!')
 
 
 
