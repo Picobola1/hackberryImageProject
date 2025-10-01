04d9616d8a5a22d443b03b267d2e3d064a254162
Ellie Bonilla <bonilla.ellie@gmail.com>
2025-09-30 20:34:52 -0700

    hehe landmarks work now

--- siege/fingertracking.py
+++ siege/fingertracking.py
@@ -5,12 +5,27 @@ import time
 
 cap = cv.VideoCapture(0)
 
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
+mpHands = mp.solutions.hands 
+hands = mpHands.Hands()# defult parmeters
 
-# run a web cam
+# run a web cam and anything while web cam is still going
 while True:
     success, img = cap.read()
 
+    imgRgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    results = hands.process(imgRgb)
+    mpDraw = mp.solutions.drawing_utils
+    #print(results.multi_hand_landmarks)
+    #check if anyhands are found
+    if results.multi_hand_landmarks:
+        #get info from each hand/ loops thru each hand
+        for handLms in results.multi_hand_landmarks:
+            #not drawing on rgb img cuz we are not displaying the img
+            # draw land marks of single hand
+            mpDraw.draw_landmarks(img, handLms)
+            
+
     cv.imshow("Image", img)
-    cv.waitKey(1)
+    key = cv.waitKey(1) & 0xFF
+    if key == ord('q'):   # press q to quit
+        break
