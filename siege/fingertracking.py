8b1e48580a2b6151a6d6eb384fae4969b2178293
Ellie Bonilla <bonilla.ellie@gmail.com>
2025-10-01 15:29:41 -0700

    coin moves

--- siege/fingertracking.py
+++ siege/fingertracking.py
@@ -1,15 +1,19 @@
 import cv2 as cv
 import numpy as np
 import mediapipe as mp
-import time
+import random
 
 cap = cv.VideoCapture(0)
 flappybird = cv.imread("siege/flappybird.png", cv.IMREAD_COLOR)
+CoinSpawingSquareSize = 10
+coin = cv.imread("siege/Coin.jpg", cv.IMREAD_COLOR)
 mpHands = mp.solutions.hands 
 hands = mpHands.Hands()# defult parmeters
 
 # run a web cam and anything while web cam is still going
 while True:
+   
+
     success, img = cap.read()
     flipped_img = cv.flip(img, 1) # Flip horizontally
     imgRgb = cv.cvtColor(flipped_img, cv.COLOR_BGR2RGB)
@@ -17,6 +21,14 @@ while True:
     mpDraw = mp.solutions.drawing_utils
     #print(results.multi_hand_landmarks)
     #check if anyhands are found
+    window_h, window_w, c = flipped_img.shape
+    #get coin width and height
+    coinH, coinW = coin.shape[:2]
+    CoinRangeX = random.randint(0,window_w//CoinSpawingSquareSize - 1) 
+    CoinRangeY = random.randint(0,window_h//CoinSpawingSquareSize - 1)
+    #print(CoinRangeX,CoinRangeY)
+                #
+    flipped_img[CoinRangeY:CoinRangeY + coinH, CoinRangeX:CoinRangeX + coinW] = coin
     if results.multi_hand_landmarks:
         #get info from each hand/ loops thru each hand
         for handLms in results.multi_hand_landmarks:
@@ -36,7 +48,7 @@ while True:
 
                     resizeBird = cv.resize(flappybird, (BirdSize,BirdSize))
                     flipped_img[y1:y1+BirdSize, x1:x1+BirdSize] = resizeBird
-                    
+          
 
     cv.imshow(" Image", flipped_img)
     key = cv.waitKey(1) & 0xFF
