4e664cb7bb0c182d1e602503c33acdce9e50375b
Ellie Bonilla <bonilla.ellie@gmail.com>
2025-10-02 20:31:19 -0700

    spawns 5 coins randomly

--- siege/fingertracking.py
+++ siege/fingertracking.py
@@ -3,32 +3,46 @@ import numpy as np
 import mediapipe as mp
 import random
 
+
 cap = cv.VideoCapture(0)
+success, img = cap.read()
+window_h, window_w, c = img.shape
 flappybird = cv.imread("siege/flappybird.png", cv.IMREAD_COLOR)
 CoinSpawingSquareSize = 10
-coin = cv.imread("siege/Coin.jpg", cv.IMREAD_COLOR)
+coinImg = cv.imread("siege/Coin.jpg", cv.IMREAD_COLOR)
 mpHands = mp.solutions.hands 
 hands = mpHands.Hands()# defult parmeters
+resizeCoin = cv.resize(coinImg, (25,25))
+coinH, coinW = resizeCoin.shape[:2]
 
+
+coin = []
+for i in range(5):
+    CoinRangeX = random.randint(0,window_w - CoinSpawingSquareSize - 50) 
+    CoinRangeY = random.randint(0,window_h - CoinSpawingSquareSize - 15)
+    coin.append((CoinRangeX,CoinRangeY))
 # run a web cam and anything while web cam is still going
 while True:
    
-
     success, img = cap.read()
     flipped_img = cv.flip(img, 1) # Flip horizontally
+    window_h, window_w, c = flipped_img.shape
+    
     imgRgb = cv.cvtColor(flipped_img, cv.COLOR_BGR2RGB)
     results = hands.process(imgRgb)
     mpDraw = mp.solutions.drawing_utils
     #print(results.multi_hand_landmarks)
     #check if anyhands are found
-    window_h, window_w, c = flipped_img.shape
+   
     #get coin width and height
-    coinH, coinW = coin.shape[:2]
-    CoinRangeX = random.randint(0,window_w//CoinSpawingSquareSize - 1) 
-    CoinRangeY = random.randint(0,window_h//CoinSpawingSquareSize - 1)
+    
+    for (CoinRangeX, CoinRangeY) in coin:
+        flipped_img[CoinRangeY:CoinRangeY+coinH, CoinRangeX:CoinRangeX+coinW] = resizeCoin
+
+        
     #print(CoinRangeX,CoinRangeY)
-                #
-    flipped_img[CoinRangeY:CoinRangeY + coinH, CoinRangeX:CoinRangeX + coinW] = coin
+    
+    flipped_img[CoinRangeY:CoinRangeY + coinH, CoinRangeX:CoinRangeX + coinW] = resizeCoin
     if results.multi_hand_landmarks:
         #get info from each hand/ loops thru each hand
         for handLms in results.multi_hand_landmarks:
